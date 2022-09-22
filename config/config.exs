4d4aeb8bcfd6933df5e986e6486dedb758b53385
Dickson <dicksonmelo@gmail.com>
2022-09-22 05:50:46 -0300

    inicindo scraper do linkedin

---
+++ config/config.exs
@@ -0,0 +1,15 @@
+import Config
+
+config :crawly,
+  closespider_timeout: 10,
+  concurrent_requests_per_domain: 1,
+  pipelines: [
+    Crawly.Pipelines.JSONEncoder,
+    {Crawly.Pipelines.WriteToFile, extension: "jl", folder: "/tmp"}
+  ],
+  middlewares: [
+    {Crawly.Middlewares.UserAgent,
+     user_agents: [
+       "Opera/9.80 (Windows NT 6.1; WOW64) Presto/2.12.388 Version/12.18"
+     ]}
+  ]
---
+++ lib/price_spider/basic_spider.ex
@@ -0,0 +1,39 @@
+defmodule BasicSpider do
+  use Crawly.Spider
+  @impl Crawly.Spider
+  def base_url do
+    "http://www.amazon.com"
+  end
+
+  @impl Crawly.Spider
+  def init() do
+    [
+      start_urls: [
+        "https://www.amazon.com/GIGABYTE-Graphics-WINDFORCE-GV-N3080GAMING-OC-12GD/dp/B09QDWGNPG/ref=sr_1_4?crid=1CLZE45WJ15HH&keywords=3080+graphics+card&qid=1650808965&sprefix=3080+%2Caps%2C116&sr=8-4",
+        "https://www.amazon.com/ZOTAC-Graphics-IceStorm-Advanced-ZT-A30800J-10PLHR/dp/B099ZCG8T5/ref=sr_1_5?crid=1CLZE45WJ15HH&keywords=3080+graphics+card&qid=1650808965&sprefix=3080+%2Caps%2C116&sr=8-5"
+      ]
+    ]
+  end
+
+  @impl Crawly.Spider
+  def parse_item(response) do
+    {:ok, document} =
+      response.body
+      |> Floki.parse_document()
+
+    price =
+      document
+      |> Floki.find(".a-box-group span.a-price span.a-offscreen")
+      |> List.first()
+      |> Floki.text()
+      |> String.trim_leading()
+      |> String.trim_trailing()
+
+    %Crawly.ParsedItem{
+      :items => [
+        %{price: price, url: response.request_url}
+      ],
+      :requests => []
+    }
+  end
+end
